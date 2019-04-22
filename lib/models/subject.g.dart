09ddede75220d41d97760c1290bbc51c5621230c
Dimitar Petrov <buldozer9993@gmail.com>
2019-04-22 22:20:46 +0300

    Subject models

---
+++ lib/models/subject.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'subject.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Subject _$SubjectFromJson(Map<String, dynamic> json) {
+  return Subject(
+      title: json['title'] as String,
+      description: json['description'] as String,
+      topics: (json['topics'] as List)
+          ?.map((e) =>
+              e == null ? null : Topic.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$SubjectToJson(Subject instance) => <String, dynamic>{
+      'title': instance.title,
+      'description': instance.description,
+      'topics': instance.topics
+    };
---
+++ lib/models/subject_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'subject_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SubjectModel _$SubjectModelFromJson(Map<String, dynamic> json) {
+  return SubjectModel(
+      subjects: (json['subjects'] as List)
+          ?.map((e) =>
+              e == null ? null : Subject.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$SubjectModelToJson(SubjectModel instance) =>
+    <String, dynamic>{'subjects': instance.subjects};
